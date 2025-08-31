3cc47735715aa41c01dd9c95148a5854afd08ee9
William Nick <williamnick44@gmail.com>
2025-08-30 22:19:36 -0400

    added new code files

---
+++ src/level.c
@@ -0,0 +1,6 @@
+#include <level.h>
+#include <>
+
+void load_level(level_t* level, const char* filename){
+
+}
---
+++ src/player.c
@@ -0,0 +1,7 @@
+#include <player.h>
+#include <SDL3/SDL.h>
+#include <chipmunk/chipmunk.h>
+
+void init_player(player_t* player, cpSpace space){
+
+}
